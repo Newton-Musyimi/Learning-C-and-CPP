5b537d8382692cc8b525185e712301cf556dcfe0
Musyimi <newtonthemaker@gmail.com>
2019-12-04 15:53:07 +0300

    Updated the README file

---
+++ Lesson1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "Hello World...Again!\nThis is Newton\nI am learning C++ using the GNU compiler\n";
+    return 0;
+}
